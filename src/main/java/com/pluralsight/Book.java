9d680ab99bbd64f959dbd4ccf5f3d2b8be5ba78f
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-07 20:05:58 -0500

    add book class and field members commit

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
 
 public class Book {
+        // field members
+        private int id;
+        private String isbn;
+        private String title;
+        private boolean isCheckedOut;
+        private String checkedOutTo;
+
 }
