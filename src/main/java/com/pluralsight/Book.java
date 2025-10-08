2d0e130a99788594aa0e7a5d235ca7baafaf8754
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-07 20:11:57 -0500

    update constructor commit

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -1,18 +1,20 @@
 package com.pluralsight;
 
 public class Book {
-        // field members
-        private int id;
-        private String isbn;
-        private String title;
-        private boolean isCheckedOut;
-        private String checkedOutTo;
+    // field members
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
 
     // Constructor
     public Book(int id, String isbn, String title) {
         this.id = id;
         this.isbn = isbn;
         this.title = title;
+        this.isCheckedOut = false; // I added isCheckedOut = false bc initially it won't be checkedOut
+        this.checkedOutTo = ""; // I added checkOutTo = "" bc the initially it will be empty since no one has checked it out yet
     }
 
     // Getters
@@ -35,13 +37,15 @@ public class Book {
     public String getCheckedOutTo() {
         return checkedOutTo;
     }
+
     // checkOut(name) Method
-    public void checkOut(String name){
+    public void checkOut(String name) {
         this.isCheckedOut = true;
         this.checkedOutTo = name;
     }
+
     // checkIn() Method
-    public void checkIn(){
+    public void checkIn() {
         this.isCheckedOut = false;
         this.checkedOutTo = "";
     }
